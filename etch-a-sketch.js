a8a8055abee24438e0ae1c6723682b619493de24
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-07 20:12:10 +0200

    Add fixed Sized sketchpad with border

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,6 @@
-const size = 100;
+const size = 8;
+
+const sketchPadSize = 960;
 
 let container = document.querySelector("#container");
 
@@ -9,11 +11,14 @@ function createGrid(size){
     for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.classList.add('row');
-        row.style.width = size*10 + "px";
+        row.style.width = sketchPadSize + "px";
+        row.style.height = sketchPadSize/size + "px";
 
         for (let j = 0; j < size; j++) {
             let col = document.createElement("div");
             col.classList.add('col');
+            col.style.width = sketchPadSize/size + "px";
+            col.style.height = sketchPadSize/size + "px";
             row.appendChild(col);
 
         }
